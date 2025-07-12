cbeb440d5be0d49ac7c2f375a2c6f4acb3782a3a
jayteemoney <jethroirmiya@gmail.com>
2025-07-12 17:52:47 +0000

    added the App file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import StakingInterface from "./components/StakingInterface";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <StakingInterface />
+    </div>
+  );
+}
+
+export default App;
